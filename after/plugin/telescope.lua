da15f719bf6b39daec42dcd8f05c08e761a99a5d
Spencer Christensen <spencer@thismoment.com>
2024-02-28 20:23:35 -0600

    add telescope-fzf + refactor telescope config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,11 +1,69 @@
+local telescope = require('telescope')
+local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>p', builtin.git_files, {})
+vim.keymap.set('n', '<leader>/', builtin.search_history, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>c', builtin.colorscheme, {})
+vim.keymap.set('n', '<leader>d', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<leader>s', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>c', builtin.grep_string, {})
--- vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-
-
--- vim.keymap.set('n', '<leader>ps', builtin.lsp_workspace_symbols, {})
+vim.keymap.set('n', '<leader>m', builtin.marks, {})
 vim.keymap.set('n', '<leader>o', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>p', builtin.git_files, {})
+vim.keymap.set('n', '<leader>s', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>w', builtin.grep_string, {})
+
+local function filename_first(_, path)
+    local tail = vim.fs.basename(path)
+    local parent = vim.fs.dirname(path)
+    if parent == "." then
+        return tail
+    end
+    return string.format("%s\t\t%s", tail, parent)
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "TelescopeResults",
+	callback = function(ctx)
+		vim.api.nvim_buf_call(ctx.buf, function()
+			vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+			vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+		end)
+	end,
+})
+
+
+telescope.setup {
+    defaults = {
+        path_display = filename_first,
+        file_ignore_patterns = { 'node_modules', 'vendor' },
+        -- layout_config = {
+        --     width = 0.7,
+        --     height = 0.4,
+        -- },
+    },
+    pickers = {
+        buffers = {
+            ignore_current_buffer = true,
+            sort_lastused = true,
+            mappings = {
+                i = {
+                    ['<c-d>'] = actions.delete_buffer + actions.move_to_top,
+                }
+            }
+        },
+        colorscheme = {
+            theme = "dropdown",
+        },
+    },
+    extensions = {
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+		}
+	}
+}
+
+telescope.load_extension('fzf')
