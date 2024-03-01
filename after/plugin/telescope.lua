832af3a92a0d97c7b5739202007a558ad42112f6
Spencer Christensen <spencer@thismoment.com>
2024-03-01 14:40:45 -0600

    update telescope config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -22,30 +22,36 @@ local function filename_first(_, path)
     return string.format("%s\t\t%s", tail, parent)
 end
 
+vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "TelescopeResults",
-	callback = function(ctx)
-		vim.api.nvim_buf_call(ctx.buf, function()
-			vim.fn.matchadd("TelescopeParent", "\t\t.*$")
-			vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
-		end)
-	end,
+    pattern = "TelescopeResults",
+    callback = function(ctx)
+        vim.api.nvim_buf_call(ctx.buf, function()
+            vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+            vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+        end)
+    end,
 })
-
 
 telescope.setup {
     defaults = {
         path_display = filename_first,
         file_ignore_patterns = { 'node_modules', 'vendor' },
-        -- layout_config = {
-        --     width = 0.7,
-        --     height = 0.4,
-        -- },
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+        }
     },
     pickers = {
         buffers = {
+            theme = "dropdown",
             ignore_current_buffer = true,
             sort_lastused = true,
+            previewer = false,
             mappings = {
                 i = {
                     ['<c-d>'] = actions.delete_buffer + actions.move_to_top,
@@ -55,15 +61,18 @@ telescope.setup {
         colorscheme = {
             theme = "dropdown",
         },
+        find_files = {
+            previewer = false,
+            theme = "dropdown",
+        },
+        git_files = {
+            previewer = false,
+            theme = "dropdown",
+        },
+        search_history = {
+            theme = "dropdown",
+        },
     },
-    extensions = {
-		fzf = {
-			fuzzy = true,
-			override_generic_sorter = true,
-            override_file_sorter = true,
-            case_mode = "smart_case",
-		}
-	}
 }
 
 telescope.load_extension('fzf')
